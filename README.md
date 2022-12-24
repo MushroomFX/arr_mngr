4aef204699ac82ab8a36f401c98061cb80cc8377
MushroomFX <dergolem26@gmail.com>
2022-12-24 15:39:59 +0100

    + Simple features added + Readme added

---
+++ README.md
@@ -0,0 +1,61 @@
+# **arr_mngr**
+
+### **Features**
++ [Rotate](#rotate) 2D-Arrays
++ [Mirror](#mirror) Arrays
++ [Shuffle](#shuffle) Arrays
++ [Sort](#sort-by) Arrays by row / column <sup>**[*WIP*]**</sup>
+#
+<br></br>
+
+### **How to use**
+
+#### Rotate
+> Rotate clockwise
+> ```
+> manage.rotate.c(array);
+> ```
+> Rotate counter clockwise
+> ```
+> manage.rotate.c(array);
+> ```
+
+#
+
+#### Mirror
+> Mirron on X-axis
+> ```
+> manage.mirror.x(array);
+> ```
+> Mirron on Y-axis
+> ```
+> manage.mirror.y(array);
+> ```
+
+#
+
+#### Shuffle
+> Shuffle an array
+> ```
+> manage.shuffle(array);
+> ```
+
+#
+
+#### Sort by
+> Sort an array by row-index
+> ```
+> manage.sortBy.row(array);
+> ```
+> Sort an array by column-index <sup>**[*WIP*]**</sup>
+> ```
+> manage.sortBy.collum(array);
+> ```
+
+#
+
+<br></br>
+
+### **Contact & help**
+> Discord: **[Mushroom#2221](discord.com/users/355012453609897985)**
+
---
+++ script.js
@@ -0,0 +1,93 @@
+var test_arr = [
+    [12,'abc',5,1],
+    [11,'def',6,2],
+    [10,'ghj',7,3],
+    [9,'ijk',8,4]
+]
+
+var manage = {
+  'mirror': {
+    y: function(arr){
+      var output = []
+      for (i=0;i<arr[0].length;i++){
+        output.push(arr[i].reverse())
+      }
+      return output
+    },
+    x: function(arr){
+      var output = arr.reverse()
+      return output
+    },
+  },
+  'rotate': {
+    cc:function(arr){
+      var array = arr.reverse()
+      var output = array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+      return output.reverse()
+      },
+    c:function(arr){
+      var array = arr[0].map((_, colIndex) => arr.map(row => row[colIndex]));
+      var output = []
+      for(i=0;i<array.length;i++){
+        output.push(array[i].reverse())
+      }
+      return output
+    },
+  },
+  shuffle: function(arr){
+    var a = arr.join().split(',')
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    var output = []
+    for(i=0;i<arr.length;i++) {
+      output.push(a.slice(i*arr[0].length,(i+1)*arr[0].length,))
+    }
+    return output;
+  },
+  'sortBy': {
+    row: function(arr, row) {
+      var output = [];
+      var list_arr = [];
+      var sortBy = [];
+      // add numbers
+      for (i = 0; i < arr[0].length; i++) {
+        var list_arr_temp = [];
+        for (j = 0; j < arr.length; j++) {
+          list_arr_temp.push(arr[i][j]);
+        }
+        list_arr_temp.push(i);
+        list_arr.push(list_arr_temp);
+      }
+      // get row
+      for (i = 0; i < arr.length; i++) {
+        sortBy.push(arr[i][row]);
+      } 
+      sortBy.sort();
+      sortBy.sort(function (a, b) {
+        return a - b;
+      });
+    
+      var temp_arr = [];
+      var rem_arr = arr;
+      for (i = 0; i < sortBy.length; i++) {
+        for (j = 0; j < sortBy.length; j++) {
+          if (rem_arr[j].indexOf(sortBy[i]) > -1) {
+            temp_arr.push(arr[j]);
+            rem_arr[j] = [];
+          }
+        }
+      }
+    
+      var output = temp_arr;
+      return output;
+    }
+  },
+  colum: function(){
+    return "Not Working Yet!\n If you still need this, do this:\n 1. rotate Array clockwise\n 2. Sort array by row\n 3. Rotate array counter clokwise"
+  }
+}
